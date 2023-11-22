fd4ca34a67b182f31c641aec85c099e54ed5195d
Grant Greenberg <19313835+grantgreenberg@users.noreply.github.com>
2023-11-22 13:05:36 -0700

    feat: added main container instead of using body, update naming conventions in js and css for clarity, and use of const over let where variable doesn't change value.

--- eas.js
+++ eas.js
@@ -1,35 +1,39 @@
 function createGrid(size = 16) {
-
-    let body = document.querySelector('body');
+    const body = document.querySelector('body');
     removeAllChildNodes(body);
-    body.classList.add('body');
 
-    let setGridSizeButton = document.createElement('button');
+    const setGridSizeButton = document.createElement('button');
     setGridSizeButton.textContent = 'Set Grid Size';
-    setGridSizeButton.addEventListener('click', () => {
-        updateGrid();
-    });
+    setGridSizeButton.addEventListener('click', updateGrid);
 
     body.appendChild(setGridSizeButton);
 
-    for (let i=0;i<size;i++) {
-        let hw = 960 / size;
-        let containerDiv = document.createElement('div');
-        containerDiv.classList.add('containerDiv');
-        containerDiv.style.height = `${hw}px`;
-        body.appendChild(containerDiv);
-        for (let j=0;j<size;j++) {
-            let itemDiv = document.createElement('div');
-            itemDiv.classList.add('itemDiv');
-            itemDiv.style.height = `${hw}px`;
-            itemDiv.style.width = `${hw}px`;
-            containerDiv.appendChild(itemDiv);
+    const containerDiv = document.createElement('div');
+    containerDiv.classList.add('grid-container');
+    body.appendChild(containerDiv);
+
+    const containerSize = 960;
+    const cellSize = containerSize / size;
+
+    for (let i = 0; i < size; i++) {
+        const rowDiv = document.createElement('div');
+        rowDiv.classList.add('grid-row');
+        rowDiv.style.height = `${cellSize}px`;
+        containerDiv.appendChild(rowDiv);
+
+        for (let j = 0; j < size; j++) {
+            const itemDiv = document.createElement('div');
+            itemDiv.classList.add('grid-item');
+            itemDiv.style.width = `${cellSize}px`;
+            itemDiv.style.height = `${cellSize}px`;
+            rowDiv.appendChild(itemDiv);
+
             itemDiv.addEventListener('mouseover', () => {
                 updateBackgroundColor(itemDiv);
             });
         }
     }
-};
+}
 
 function removeAllChildNodes(parent) {
     while (parent.firstChild) {
@@ -39,20 +43,19 @@ function removeAllChildNodes(parent) {
 
 function updateBackgroundColor(itemDiv) {
     itemDiv.style.backgroundColor = 'gray';
-};
+}
 
 function updateGrid() {
+    let size = prompt('Please enter your desired grid size: ');
 
-    let size = prompt("Please enter your desired grid size: ");
-    
     while (size > 100 || size < 1) {
-        alert('Grid size should be between 1 and 100, inclusive.')
-        size = prompt("Please enter your desired grid size: ");
+        alert('Grid size should be between 1 and 100, inclusive.');
+        size = prompt('Please enter your desired grid size: ');
     }
 
     createGrid(size);
 }
 
-window.addEventListener('load', (event) => {
+window.addEventListener('load', () => {
     createGrid();
 });
