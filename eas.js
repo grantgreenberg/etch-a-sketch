9d8b2dcea75270bfd4bd040550f9750a908b293a
Grant Greenberg <19313835+grantgreenberg@users.noreply.github.com>
2023-11-22 12:20:10 -0700

    feat: updated js to have itemDiv color change on hover and button that allows changing of grid size.

--- eas.js
+++ eas.js
@@ -1,7 +1,16 @@
 function createGrid(size = 16) {
 
     let body = document.querySelector('body');
+    removeAllChildNodes(body);
     body.setAttribute('style', 'max-width: 960px; margin: auto;');
+
+    let setGridSizeButton = document.createElement('button');
+    setGridSizeButton.textContent = 'Set Grid Size';
+    setGridSizeButton.addEventListener('click', () => {
+        updateGrid();
+    });
+
+    body.appendChild(setGridSizeButton);
 
     for (let i=0;i<size;i++) {
         let hw = 960 / size;
@@ -13,12 +22,34 @@ function createGrid(size = 16) {
             itemDiv.setAttribute('style', `flex: 1; border: 1px solid black; height: ${hw}px; width: ${hw}px; margin: 0; padding: 0;`)
             containerDiv.appendChild(itemDiv);
             itemDiv.addEventListener('mouseover', () => {
-                itemDiv.setAttribute('style', `flex: 1; border: 1px solid black; height: ${hw}px; width: ${hw}px; margin: 0; padding: 0; background-color: black;`)
+                updateBackgroundColor(itemDiv);
             });
         }
     }
+};
+
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+function updateBackgroundColor(itemDiv) {
+    itemDiv.style.backgroundColor = 'gray';
+};
+
+function updateGrid() {
+
+    let size = prompt("Please enter your desired grid size: ");
+    
+    while (size > 100 || size < 1) {
+        alert('Grid size should be between 1 and 100, inclusive.')
+        size = prompt("Please enter your desired grid size: ");
+    }
+
+    createGrid(size);
 }
 
 window.addEventListener('load', (event) => {
-    createGrid(32);
+    createGrid();
 });
