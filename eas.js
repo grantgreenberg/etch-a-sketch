33c58d343e1bfd9a5c9b40c7a74a27a979d663f3
Grant Greenberg <19313835+grantgreenberg@users.noreply.github.com>
2023-11-21 21:24:57 -0700

    feat: add stylesheet and replaced some stylesheet pieces from js file.

--- eas.js
+++ eas.js
@@ -1,12 +1,12 @@
 function createGrid(size = 16) {
 
     let body = document.querySelector('body');
-    body.setAttribute('style', 'max-width: 960px; margin: auto;');
+    body.classList.add('body');
 
     for (let i=0;i<size;i++) {
         let hw = 960 / size;
         let containerDiv = document.createElement('div');
-        containerDiv.setAttribute('style', `display: flex; height: ${hw}; margin: 0; padding: 0;`);
+        containerDiv.style.height = ${hw};
         body.appendChild(containerDiv);
         for (let j=0;j<size;j++) {
             let itemDiv = document.createElement('div');
